7b1ace11fd78b5d7a39b13a4007a4d6582b6e118
Sorongos <dosora9@gmail.com>
2023-03-02 21:55:01 +0900

    loadImage 불러오기 구현
    uriList로 이미지를 가져옴
    launch로 모든 이미지 파일을 가져옴

--- app/src/main/java/com/sorongos/gallery/MainActivity.kt
+++ app/src/main/java/com/sorongos/gallery/MainActivity.kt
@@ -3,10 +3,12 @@ package com.sorongos.gallery
 import android.Manifest
 import android.content.ContentValues.TAG
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -14,38 +16,54 @@ import com.sorongos.gallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    /**사진을 찍어서, 비디오를 찍어서 클립을 가져올 수 있음
+     * startactivity : 다른 앱에서 열기
+     * 사진을 여러 장 받아옴
+     * */
+    private val imageLoadLauncher =
+        registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList ->
+            //urilist로 통해 이미지 업데이트
+            updateImages(uriList)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.loadImageButton.setOnClickListener {
-//            checkPermission()
-            Log.d(TAG,"clicked")
-            ActivityCompat.requestPermissions(this,
-            arrayOf(Manifest.permission.READ_MEDIA_IMAGES),
-            REQUEST_READ_EXTERNAL_STORAGE)
+            checkPermission()
+            Log.d(TAG, "clicked")
         }
     }
 
+    /**uri를 통해서 이미지를 보여줄 수 있다.*/
+    private fun updateImages(uriList: List<Uri>){
+        Log.i("updateImages","$uriList")
+    }
     private fun checkPermission() {
         when {
             ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.READ_MEDIA_IMAGES
             ) == PackageManager.PERMISSION_GRANTED -> {
+                //권한 모두 승인
                 loadImage()
             }
             shouldShowRequestPermissionRationale(
-                Manifest.permission.READ_EXTERNAL_STORAGE
+                Manifest.permission.READ_MEDIA_IMAGES
             ) -> {
+                //권한 승인 되기 전 권한에 대한 설명
                 showPermissionInfoDialog()
             }
             else -> {
+                //권한 승인 되기 전, 안드로이드 자체 권한 승인 팝업
                 requestReadExternalStorage()
             }
         }
     }
+
     private fun showPermissionInfoDialog() {
         AlertDialog.Builder(this).apply {
             setMessage("이미지를 가져오기 위해서, 외부 저장소 읽기 권한이 필요합니다.")
@@ -55,15 +73,19 @@ class MainActivity : AppCompatActivity() {
             }
         }.show()
     }
+
     private fun requestReadExternalStorage() {
         ActivityCompat.requestPermissions(
             this,
-            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+            arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES),
             REQUEST_READ_EXTERNAL_STORAGE
         )
     }
+
+    /**모든 이미지 타입을 가져옴
+     * saf 기능을 이용할 때 MIME 타입을 이용해 이미지 파일을 불러 오겠다*/
     private fun loadImage() {
-        Log.d(TAG,"loadImage")
+        imageLoadLauncher.launch("image/*")
     }
 
     /**request 코드*/
